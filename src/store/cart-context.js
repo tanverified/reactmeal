1711d526d91cc7f76edbf313c204bac660079d5f
tanverified <khantanveer2u@gmail.com>
2021-11-04 10:05:51 +0530

    completed

---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import { createContext } from 'react';
+
+const CartContext = createContext({
+    items: [],
+    totalAmount:0,
+    addItem: (item)=>{},
+    removeItem: (id)=>{}
+});
+
+export default CartContext;
